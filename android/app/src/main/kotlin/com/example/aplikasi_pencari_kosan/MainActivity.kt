62390ba063515ac187c1c6a4fdcd29c425e72d69
Richo Fedhia <fedhia64@gmail.com>
2021-03-18 18:52:18 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/aplikasi_pencari_kosan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aplikasi_pencari_kosan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:aplikasi_pencari_kosan/widgets/city_card.dart';
+import 'package:flutter/material.dart';
+import 'package:aplikasi_pencari_kosan/theme.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: edge,
+          ),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: edge),
+                child: Text(
+                  'Explore Now',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: edge),
+                child: Text(
+                  'Mencari Kosan yang cozy',
+                  style: greyTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: edge),
+                child: Text(
+                  'Popular Cities',
+                  style: regularTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                height: 150,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    CityCard(),
+                    CityCard(),
+                    CityCard(),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
